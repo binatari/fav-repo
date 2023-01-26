c45b7b57330a0345280063dc6ebdfd96f728d5f4
Bina Akene <akenebina@gmail.com>
2023-01-26 11:41:51 +0100

    add project page

---
+++ pages/projects/apple-music-lean-ui.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const musicUi = () => {
+  return (
+    <div>apple-music-lean-ui</div>
+  )
+}
+
+export default musicUi
---
+++ pages/projects/the-creators-hub.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const creators = () => {
+  return (
+    <div>the-creators-hub</div>
+  )
+}
+
+export default creators
