56ef96af34df1188cec66195c66eb1d6ba851e9b
Bina Akene <akenebina@gmail.com>
2023-02-05 02:31:52 +0100

    add dark mode

--- components/sections/Services.js
+++ components/sections/Services.js
@@ -3,18 +3,21 @@ import React from "react";
 const cards = [
   {
     image: "/assets/semi-circle.svg",
+    dark_image: "/assets/semi-circle-dark.svg",
     head: "What i can do for you",
     body: "Bring products to life, using a user-centered design approach. Here are the services I provide:",
     list: ["UI/UX Design", "Graphic Design", "Brand Design"],
   },
   {
     image: "/assets/triangle.svg",
+    dark_image: "/assets/triangle-dark.svg",
     head: "Applications I’m fluent in",
     body: "Every designer needs the right tools to get the job done. Here are a few of my go-to applications:",
     list: ["Figma", "Adobe Creative Suite", "Canva"],
   },
   {
     image: "/assets/ball-head.svg",
+    dark_image: "/assets/ball-head-dark.svg",
     head: "What you can expect",
     body: "I don’t just design beautiful products,I also ensure the best user experiences. Users first, design second.",
     list: ["Innovative and usable products", "Creative visual designs", "Iconic brand identities"],
@@ -29,9 +32,10 @@ const Services = () => {
       </h3>
 <div className="flex justify-center items-center gap-8">
 {
-        cards.map(({image, head, body, list}, i)=>(
+        cards.map(({image, head, body, list, dark_image}, i)=>(
           <div key={i} className='w-1/3'>
-            <img src={image} className='h-[4rem]'/> 
+            <img src={image} className='h-[4rem] dark:hidden'/> 
+            <img src={dark_image} className='h-[4rem] hidden dark:block'/> 
             <h4 className="text-[21px] font-bold mt-[18px] mb-[11px]">{head}</h4>
             <p className="mb-[11px]">{body}</p>
             <ul>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
-import Layout from '../components/Layout/Layout'
-import '../styles/globals.css'
-import { ThemeProvider } from 'tailwind-darkmode-toggle';
-
+import Layout from "../components/Layout/Layout";
+import "../styles/globals.css";
+import { ThemeProvider } from "tailwind-darkmode-toggle";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-
-  return <ThemeProvider><Layout><Component {...pageProps} /></Layout> </ThemeProvider> 
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="shortcut icon" href="/assets/fave.png" />
+        <title>Favour Momoh</title>
+      </Head>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>{" "}
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
