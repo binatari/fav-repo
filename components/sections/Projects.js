080e02751c11e7db4ed6696dbc4122269f93df03
Bina Akene <akenebina@gmail.com>
2023-02-05 03:27:59 +0100

    remove framer

--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from "framer-motion";
 import Link from "next/link";
 
 const Projects = ({ noHead, noTitle }) => {
@@ -19,7 +18,7 @@ const Projects = ({ noHead, noTitle }) => {
         <div className="w-1/2 flex justify-center">
           <Link href={"/projects/subly"}>
             <div className="bg-[#4A10C0] mx-2 rounded-3xl p-12 flex justify-center items-center">
-              <motion.img src="/assets/phone.svg" />
+              <img src="/assets/phone.svg" />
             </div>
             <p className="text-lg mt-5 font-semibold mx-2">
               Subscription tracking made easy with Sub.ly{" "}
@@ -32,7 +31,7 @@ const Projects = ({ noHead, noTitle }) => {
         <div className="w-1/2">
           <Link href={"/projects/collaborative-playlists"}>
             <div className="flex items-center flex-col justify-center mx-4 h-full">
-              <motion.img src="/assets/music.png" />
+              <img src="/assets/music.png" />
               <p className="text-lg mt-5 font-semibold w-2/3 mx-2">
                 Introducing collaborative playlists on Apple Music{" "}
                 <span className="block text-sm text-[#777777]">
@@ -44,7 +43,7 @@ const Projects = ({ noHead, noTitle }) => {
         </div>
         <div className="w-full my-[126px] flex flex-col items-center justify-center">
           <div className="w-full max-w-[1200px]">
-            <motion.img src="/assets/comi.png" className="w-full" />
+            <img src="/assets/comi.png" className="w-full" />
           </div>
           <p className="text-lg max-w-[1200px] w-full mt-5 font-semibold mx-2">
             Ticket Managing App: Improving the ticket managing experience for
@@ -57,7 +56,7 @@ const Projects = ({ noHead, noTitle }) => {
         <div className="w-1/2 flex justify-center">
           <Link href={"/projects/creators-hub"}>
             <div className="mx-4">
-              <motion.img src="/assets/laptop.png" />
+              <img src="/assets/laptop.png" />
             </div>
             <p className="text-lg mt-5 font-semibold mx-2">
               The Creators Hub{" "}
@@ -70,7 +69,7 @@ const Projects = ({ noHead, noTitle }) => {
         <div className="w-1/2 flex justify-center">
           <Link href={"/graphics"}>
             <div className="mx-4">
-              <motion.img src="/assets/pencil.png" />
+              <img src="/assets/pencil.png" />
             </div>
             <p className="text-lg mt-5 font-semibold mx-2">
               Graphic & Brand Designs{" "}
